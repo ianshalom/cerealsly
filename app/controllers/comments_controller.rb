f6c678df3cb99dadde0f4e4361d46d973f9c3f4b
Ianshalom <you@example.comianshalom92@gmail.comgit config --global user.email you@example.comianshalom92@gmail.com>
2020-05-19 17:15:24 +0800

    Updated comments controller with actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,7 @@
 class CommentsController < ApplicationController
-  def index
-  end
 
   def show
+    @comment = Comment.find(params[:id])
   end
 
   def new
@@ -12,11 +11,23 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @comment = Comment.new(comments_params)
+    @comment.save
+    redirect_to root_path
   end
 
   def update
+    @comment = Comment.find(params[:id])
+    @comment.update
+    redirect_to root_path
   end
 
   def destroy
   end
+
+  private
+  def comments_params
+    params.require(:comment).permit(:content, :post_id)
+  end
+
 end
