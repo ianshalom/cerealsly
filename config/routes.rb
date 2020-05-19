d3698727888a32c83bc883ccb4fe6872cc344954
vigbit <vigneshb@live.com>
2020-05-19 16:33:51 +0800

    "devise-implementation w/username"

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get '/posts' => 'posts#index', as: 'posts'
   get '/posts/new' => 'posts#new', as: 'new_post'
   post '/posts' => 'posts#create'
-  get '/posts/:id' => 'posts#show' , as: 'park'
+  get '/posts/:id' => 'posts#show' , as: 'post'
   get '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
   patch '/posts/:id' => 'posts#update'
   delete '/posts/:id' => 'posts#destroy'
