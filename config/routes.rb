dbf4d95013cf250322a4b7139a00ea533eddc0d1
Ianshalom <you@example.comianshalom92@gmail.comgit config --global user.email you@example.comianshalom92@gmail.com>
2020-05-19 15:40:20 +0800

    Basic crud layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'posts#index'
+  get '/posts' => 'posts#index', as: 'posts'
+  get '/posts/new' => 'posts#new', as: 'new_post'
+  post '/posts' => 'posts#create'
+  get '/posts/:id' => 'posts#show' , as: 'park'
+  get '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
+  patch '/posts/:id' => 'posts#update'
+  delete '/posts/:id' => 'posts#destroy'
+
+  get '/comments' => 'comments#index', as: 'comments'
+  get '/comments/new' => 'comments#new', as: 'new_comment'
+  post '/comments' => 'comments#create'
+  get '/comments/:id' => 'comments#show' , as: 'comment'
+  get '/comments/:id/edit' => 'comments#edit', as: 'edit_comment'
+  patch '/comments/:id' => 'comments#update'
+  delete '/comments/:id' => 'comments#destroy'
+
 end
