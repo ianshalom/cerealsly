0d71611fe53a12d2a3b0a0d09bc6ef370ea88276
Ianshalom <you@example.comianshalom92@gmail.comgit config --global user.email you@example.comianshalom92@gmail.com>
2020-05-19 16:45:42 +0800

    Created Pages controller to add landing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'posts#index'
+
+  root 'pages#home'
+
+
   get '/posts' => 'posts#index', as: 'posts'
   get '/posts/new' => 'posts#new', as: 'new_post'
   post '/posts' => 'posts#create'
