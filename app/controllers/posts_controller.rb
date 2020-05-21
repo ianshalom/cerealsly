3680e63ff8506e06775569ec3930a52156b13400
YusofGotboudine <yusof95@hotmail.com>
2020-05-21 22:56:30 +0800

    Sorted post & comments by created at

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(:created_at).reverse
     @comments = Comment.all
     @users = User.all
     @user = current_user
