5a45d3054eccea97cd7b54aa4927ed9da7a46036
Ianshalom <you@example.comianshalom92@gmail.comgit config --global user.email you@example.comianshalom92@gmail.com>
2020-05-22 18:01:03 +0800

    Fixed redirects from post and edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,8 @@ class ApplicationController < ActionController::Base
    end
 
 private
-# Overwriting the sign_out redirect path method
-def after_sign_out_path_for(resource_or_scope)
-root_path
-end
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+  root_path
+  end
 end
