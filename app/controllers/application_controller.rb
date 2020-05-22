bb219bcb8c37ab9509d88591462af6fc4fc1f2a5
Ianshalom <you@example.comianshalom92@gmail.comgit config --global user.email you@example.comianshalom92@gmail.com>
2020-05-22 17:01:57 +0800

    Added show post design layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,16 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
+
   protected
    def configure_permitted_parameters
      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :username ) }
      devise_parameter_sanitizer.permit(:account_update) { |u|    u.permit(:email, :password, :current_password, :username) }
    end
+
+private
+# Overwriting the sign_out redirect path method
+def after_sign_out_path_for(resource_or_scope)
+root_path
+end
 end
